a01d3dde36f01f9707884a0d569d1c02a12e3a18
Happy-Love <yutabox@mail.ru>
2020-05-28 11:47:07 +0300

    Init Falling-Things

---
+++ Falling-Things/Assets/Scripts/Box/Box.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 3 типа коробок
+public enum BoxType
+{
+    Fire=1,
+    Frost=2,
+    Poison=3
+}
+public class Box : MonoBehaviour
+{
+    private int hp;
+    private BoxType type;
+}
